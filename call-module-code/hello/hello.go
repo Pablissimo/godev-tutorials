dec6e1051065539789c28ca0730582a8b399ea25
Paul O'Neill <hello@pablissimo.com>
2026-02-09 11:17:32 +0000

    Basic error handling

--- call-module-code/hello/hello.go
+++ call-module-code/hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	// Disable time, source file and line number
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		// Will exit
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
